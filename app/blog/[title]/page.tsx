8e0144a7a71ca27a841bf719c18d4c4a086ead51
Adarsh Desai <desaiadarsh22@gmail.com>
2023-05-08 12:03:42 +0530

    setting up the project

---
+++ app/blog/[title]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const page = () => {
+  return <div>Blog page</div>;
+};
+
+export default page;
---
+++ app/blog/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const page = () => {
+  return <div>Blogs page</div>;
+};
+
+export default page;
