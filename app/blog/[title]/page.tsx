3eff1d709ea7727195d02f2fe8a80a506a74ca2b
Adarsh Desai <desaiadarsh22@gmail.com>
2023-05-09 11:52:35 +0530

    Fresh start

--- app/blog/[title]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface Props {}
-
-const page = () => {
-  return <div>Blog page</div>;
-};
-
-export default page;
--- app/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface Props {}
-
-const page = () => {
-  return <div>Contact form</div>;
-};
-
-export default page;
